122901a1c692368231ec18ba3a282169169d7008
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-03-30 15:27:00 +0700

    init bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavbarComponent />
   </div>
 </template>
 
 <script>
+import NavbarComponent from '@/components/Navbar.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    NavbarComponent
   }
 }
 </script>
