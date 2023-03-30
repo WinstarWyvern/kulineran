4b635a7131340788a25b066a79eba2ef4d6fe5a5
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-03-30 16:56:47 +0700

    Add image

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex justify-content-evenly align-items-center">
+    <div class="col text-center">
+        <h2><strong>Delicious & Cheap Food Menu</strong>,</h2>
+        <h2>In Your Gadget</h2>
+        <p>Ayo Segera Pesan Makananmu</p>
+        <router-link class="btn btn-lg-success">Pesan</router-link>
+    </div>
+    <div class="col">
+        <img src="../assets/images/ilustrasi.png" width="100%" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroComponent",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
     <NavbarComponent />
+    <div class="container">
+      <hero-component />
+    </div>
   </div>
 </template>
 
 <script>
 import NavbarComponent from "@/components/Navbar.vue";
+import HeroComponent from "@/components/Hero.vue";
 // @ is an alias to /src
 
 export default {
   name: "HomeView",
   components: {
     NavbarComponent,
+    HeroComponent,
   },
 };
 </script>
