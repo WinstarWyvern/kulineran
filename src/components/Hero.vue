cf46f4ee93a9ae68da77fd1748029903f149661d
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-03-30 22:22:44 +0700

    Add food card

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="d-flex justify-content-evenly align-items-center">
-    <div class="col text-center">
-        <h2><strong>Delicious & Cheap Food Menu</strong>,</h2>
-        <h2>In Your Gadget</h2>
-        <p>Ayo Segera Pesan Makananmu</p>
-        <router-link class="btn btn-lg-success">Pesan</router-link>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row d-flex justify-content-evenly align-items-center mt-4">
+        <div class="col-md-6 text-center">
+          <h2><strong>Delicious & Cheap Food Menu</strong>,</h2>
+          <h2>In Your Gadget</h2>
+          <p>Ayo Segera Pesan Makananmu</p>
+          <button class="btn btn-lg btn-success"><b-icon-arrow-right /> Pesan</button>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/ilustrasi.png" width="100%" alt="" />
+        </div>
+      </div>
     </div>
-    <div class="col">
-        <img src="../assets/images/ilustrasi.png" width="100%" alt="">
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row d-flex justify-content-evenly align-items-center mt-4">
+        <div class="col-md-6">
+          <img src="../assets/images/ilustrasi.png" width="100%" alt="" />
+        </div>
+        <div class="col-md-6 text-center">
+          <h2><strong>Delicious & Cheap Food Menu</strong>,</h2>
+          <h2>In Your Gadget</h2>
+          <p>Ayo Segera Pesan Makananmu</p>
+          <button class="btn btn-lg btn-success"><b-icon-arrow-right /> Pesan</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,5 +46,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,56 @@
     <NavbarComponent />
     <div class="container">
       <hero-component />
+
+      <div class="row mt-4 mb-4">
+        <div class="col">
+          <h2>Best <strong>Food</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-lg btn-success float-end">
+            <b-icon-eye-fill /> Lihat Semua
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-md-4" v-for="product in products" :key="product.id">
+          <CardProductComponent :product=product />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
 import NavbarComponent from "@/components/Navbar.vue";
 import HeroComponent from "@/components/Hero.vue";
-// @ is an alias to /src
+import CardProductComponent from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
     NavbarComponent,
     HeroComponent,
+    CardProductComponent,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => this.setProduct(response.data))
+      .catch((error) => console.log(error));
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
   },
 };
 </script>
